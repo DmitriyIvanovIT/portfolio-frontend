c4d8bd8c4bf2183414bb71feb5d2947a7238b182
DmitriyIvanovIT <strela199720@gmail.com>
2020-09-14 00:23:47 +0300

    Updates

---
+++ precache-manifest.410375bb1a9392e66a179185dff9de17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc934807fbae31cd76958282024ad32",
+    "url": "/portfolio-frontend/index.html"
+  },
+  {
+    "revision": "fe318d9a61b76cde0d32",
+    "url": "/portfolio-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fe318d9a61b76cde0d32",
+    "url": "/portfolio-frontend/static/js/2.bffd553a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-frontend/static/js/2.bffd553a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deca06264add6a916b75",
+    "url": "/portfolio-frontend/static/js/main.19238cfd.chunk.js"
+  },
+  {
+    "revision": "0073d22dad1008804034",
+    "url": "/portfolio-frontend/static/js/runtime-main.ce51a497.js"
+  },
+  {
+    "revision": "07499ae9bb5de4a565a0de1451b77f5e",
+    "url": "/portfolio-frontend/static/media/1533622641_85be1d0bf982000f887099fc7b15bcc1__1440x.07499ae9.jpg"
+  },
+  {
+    "revision": "190f10e67afdff22b5a9f82f7f1c76d9",
+    "url": "/portfolio-frontend/static/media/avatar.190f10e6.jpg"
+  }
+]);
